0ba1dde72cec5760abcb43b7e67273f97e4cc906
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2024-12-07 02:02:53 +0530

    Removed Annon Adhar

--- web/src/pages/dashboard/DashboardPage.tsx
+++ web/src/pages/dashboard/DashboardPage.tsx
@@ -1,22 +1,6 @@
-import React, { useEffect } from 'react'
-import { AnonAadhaarProvider } from "@anon-aadhaar/react";
-
-import { LogInWithAnonAadhaar, useAnonAadhaar } from "@anon-aadhaar/react";
 function DashboardPage() {
-  const [anonAadhaar] = useAnonAadhaar();
-
-  useEffect(() => {
-    console.log("Anon Aadhaar status: ", anonAadhaar.status);
-  }, [anonAadhaar]);
   return (
-    <div>
-      <AnonAadhaarProvider>
-        <div>
-          <h1>Dashboard</h1>
-          <LogInWithAnonAadhaar nullifierSeed={1234} />
-        </div>
-      </AnonAadhaarProvider>
-    </div>
+    <div>DashboardPage</div>
   )
 }
 
